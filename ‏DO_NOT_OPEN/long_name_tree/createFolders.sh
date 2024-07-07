0b4bc9765512c05344d1b7439e1aa1a6edaad8eb
StringManolo <manuelvarelacaldas@gmail.com>
2024-07-07 05:06:51 +0000

    50 folders 1326 chars

--- ‏DO_NOT_OPEN/long_name_tree/createFolders.sh
+++ ‏DO_NOT_OPEN/long_name_tree/createFolders.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 
-# Verificar si se ha pasado un argumento
-if [ $# -ne 1 ]; then
-  echo "Uso: $0 <número de carpetas>"
-  exit 1
-fi
-
-# Número de carpetas a crear
 n=$1
 
-# Inicializar variables
 current_dir="."
-folder_name="a"
+folder_name="$2"
 
-# Crear carpetas con nombres cada vez más largos
 for ((i=1; i<=n; i++)); do
   current_dir="$current_dir/$folder_name"
   mkdir -p "$current_dir"
-  folder_name="${folder_name}a"
+  folder_name="${folder_name}$2"
 done
 
-# Calcular la longitud total de todas las carpetas
 length_total=${#current_dir}
 
-# Crear el archivo X.txt al final del todo
 touch "$current_dir/${length_total}.txt"
 
 echo "Carpetas creadas hasta: $current_dir"
